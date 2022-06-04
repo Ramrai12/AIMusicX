86b998e723213f95163606de562163a5afc3c83b
Dhrubamoy <97961618+LEGENDARY-OS@users.noreply.github.com>
2022-06-04 15:09:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ admins = {}
 
 API_ID = int(getenv("API_ID", "15235059"))
 API_HASH = getenv("API_HASH", "352269b9e97005ffbe7f577dd89911e6")
-BOT_TOKEN = getenv("BOT_TOKEN","5257312176:AAFqDe_Q81xkv3u9SCF_i0vIfIQfu6rZxRI")
-BOT_NAME = getenv("BOT_NAME","itscrazy2_bot")
+BOT_TOKEN = getenv("BOT_TOKEN","5260589630:AAGGopcuJ6Uh8kFT1Uy1bruaEQJ2HMkURCI")
+BOT_NAME = getenv("BOT_NAME","Itscrazy3_bot")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "120"))
 SESSION_NAME = getenv("SESSION_NAME", "BQCE9Ll8CjjHsWVLGDYI4YKStjrzFfJDDZdMl89W-9cRnHVycMki0hdDEVJQ0UE0wUniTap9MkpmimOHfElY9kP9ZyDO_gR3mWwy-qZVekvcJpTI1WnSMw0XZ4ik8Xbayb7_o4J5cPXpni6qcqPSnx418fV_HHiKohtc_bQoAwfAcfNKL0n2E_zadlMeQeT0giEuboiPsKGfeYgSdqo6lll9G0gK44lOeX0vsLC6vuKASUkfQqQtJnFNknox_BqMsxYsvZQeEnRLzlUvmtfb5bWiC68nFGR1WE1oaAdfj4CY-FcA48IY1L-RxC6mCSk6BgW8Fn2Ho-7KiF6FYt8TyRHxbmUSywA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
